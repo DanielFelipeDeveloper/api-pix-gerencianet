8e6240f24034c9bba984cba5917c2c130d5ca436
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-06-17 19:41:50 -0400

    feat: add express

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
-
 
 const cert = fs.readFileSync(
   path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
@@ -14,47 +14,57 @@ const cert = fs.readFileSync(
 
 const agent = new https.Agent({
   pfx: cert,
-  passphrase: ''
+  passphrase: "",
 });
 
 const credentials = Buffer.from(
   `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
-).toString('base64')
+).toString("base64");
 
-axios({
-  method: 'POST',
-  url: `${process.env.GN_ENDPOINT}/oauth/token`,
-  headers: {
-    Authorization: `Basic ${credentials}`,
-    'Content-Type': 'application/json'
-  },
-  httpsAgent: agent,
-  data: {
-    grant_type: 'client_credentials'
-  }
-}).then(response => {
-  const accessToken = response.data?.access_token;
+const app = express();
 
-  const reqGN = axios.create({
-    baseURL: process.env.GN_ENDPOINT,
-    httpsAgent: agent,
+app.set("view engine", "ejs");
+app.set('views', 'src/views');
+
+app.get('/', (req, res) => {
+  axios({
+    method: "POST",
+    url: `${process.env.GN_ENDPOINT}/oauth/token`,
     headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'Content-Type': 'application/json'
-    }
-  });
-
-  const dataCob = {
-    calendario: {
-      expiracao: 3600
+      Authorization: `Basic ${credentials}`,
+      "Content-Type": "application/json",
     },
-    valor: {
-      original: '85.00'
+    httpsAgent: agent,
+    data: {
+      grant_type: "client_credentials",
     },
-    chave: '5e3f88b4-0d0a-4ef0-b8a5-20ee247cdb4b',
-    solicitacaoPagador: 'Cobrança dos serviços prestados'
-  };
-
+  }).then((response) => {
+    const accessToken = response.data?.access_token;
   
-  reqGN.post('/v2/cob', dataCob).then(console.log);
+    const reqGN = axios.create({
+      baseURL: process.env.GN_ENDPOINT,
+      httpsAgent: agent,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+  
+    const dataCob = {
+      calendario: {
+        expiracao: 3600,
+      },
+      valor: {
+        original: "85.00",
+      },
+      chave: "5e3f88b4-0d0a-4ef0-b8a5-20ee247cdb4b",
+      solicitacaoPagador: "Cobrança dos serviços prestados",
+    };
+  
+    reqGN.post("/v2/cob", dataCob).then(response => res.send(response.data));
+  });
+});
+
+app.listen(8000, () => {
+  console.log('running');
 });
