02250ab001ae0e7767cd0c5a2eefd1c458363d9a
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-06-18 06:26:40 -0400

    refactoring code

--- src/server.js
+++ src/server.js
@@ -1,69 +1,34 @@
-const express = require("express");
-const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
-const https = require("https");
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-const cert = fs.readFileSync(
-  path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
-);
-
-const agent = new https.Agent({
-  pfx: cert,
-  passphrase: "",
-});
-
-const credentials = Buffer.from(
-  `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
-).toString("base64");
+const express = require('express');
+const GNRequest = require('./apis/gerencianet')
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
-app.get('/', (req, res) => {
-  axios({
-    method: "POST",
-    url: `${process.env.GN_ENDPOINT}/oauth/token`,
-    headers: {
-      Authorization: `Basic ${credentials}`,
-      "Content-Type": "application/json",
+const reqGNAlready = GNRequest();
+
+app.get('/', async (req, res) => {
+  const reqGN = await reqGNAlready;
+
+  const dataCob = {
+    calendario: {
+      expiracao: 3600,
     },
-    httpsAgent: agent,
-    data: {
-      grant_type: "client_credentials",
+    valor: {
+      original: '85.00',
     },
-  }).then((response) => {
-    const accessToken = response.data?.access_token;
-  
-    const reqGN = axios.create({
-      baseURL: process.env.GN_ENDPOINT,
-      httpsAgent: agent,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-    });
-  
-    const dataCob = {
-      calendario: {
-        expiracao: 3600,
-      },
-      valor: {
-        original: "85.00",
-      },
-      chave: "5e3f88b4-0d0a-4ef0-b8a5-20ee247cdb4b",
-      solicitacaoPagador: "Cobrança dos serviços prestados",
-    };
-  
-    reqGN.post("/v2/cob", dataCob).then(response => res.send(response.data));
-  });
+    chave: '5e3f88b4-0d0a-4ef0-b8a5-20ee247cdb4b',
+    solicitacaoPagador: 'Cobrança dos serviços prestados',
+  };
+
+  const cobResponse = await reqGN.post('/v2/cob', dataCob);
+
+  const qrCodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`);
+
+  return res.render('qrcode', { qrCodeImage: qrCodeResponse.data.imagemQrcode });
 });
+
 
 app.listen(8000, () => {
   console.log('running');
